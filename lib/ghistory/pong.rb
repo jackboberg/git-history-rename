a61a8d14e2e9190b25ec15d0a885dc5ec3177245
Jack Boberg <hello@jackboberg.info>
2021-08-03 14:22:51 +0200

    feat: Change history

--- lib/ghistory/pong.rb
+++ lib/ghistory/pong.rb
@@ -10,16 +10,6 @@ module Ghistory
       <<~DOC
         This block exists so that we can edit some lines.
 
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-        incididunt ut labore et dolore magna aliqua. Sit amet justo donec enim diam
-        vulputate. Placerat vestibulum lectus mauris ultrices eros in cursus. Elementum
-        integer enim neque volutpat ac. Quisque non tellus orci ac auctor augue mauris
-        augue neque. Viverra nam libero justo laoreet sit. Ultricies mi eget mauris
-        pharetra et ultrices neque ornare aenean. Justo donec enim diam vulputate ut
-        pharetra sit. Eget est lorem ipsum dolor. Diam vulputate ut pharetra sit amet
-        aliquam id. Eget est lorem ipsum dolor sit amet. At volutpat diam ut venenatis
-        tellus in. Nullam eget felis eget nunc lobortis mattis aliquam.
-
         Volutpat lacus laoreet non curabitur gravida arcu ac tortor dignissim. Curabitur
         vitae nunc sed velit dignissim sodales ut. Netus et malesuada fames ac turpis
         egestas integer eget. Vulputate ut pharetra sit amet aliquam. Eu ultrices vitae
